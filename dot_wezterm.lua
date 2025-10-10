376837b4a42ccbdb39cda20e3e20d557ecd1637e
itoooak <101097771+itoooak@users.noreply.github.com>
2025-10-10 23:35:04 +0900

    Add mac config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,29 +1,31 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({
-  { family = 'Cica' },
+    { family = "Cica" },
 })
-config.font_size = 16
 
-config.color_scheme = 'iceberg-dark'
+config.color_scheme = "iceberg-dark"
 config.window_background_opacity = 0.9
 
 config.use_fancy_tab_bar = false
 
-if string.find(wezterm.target_triple, 'windows') then
-  config.default_prog = {
-    'C:/Program Files/PowerShell/7/pwsh.exe',
-    '-WorkingDirectory',
-    '~'
-  }
-
-  config.keys = {
-    { key = 'm', mods = 'CTRL', action = wezterm.action.ShowLauncher },
-  }
+if string.find(wezterm.target_triple, "windows") then
+    config.default_prog = {
+        "C:/Program Files/PowerShell/7/pwsh.exe",
+        "-WorkingDirectory",
+        "~",
+    }
+    config.keys = {
+        { key = "m", mods = "CTRL", action = wezterm.action.ShowLauncher },
+    }
+    config.font_size = 16
+elseif string.find(wezterm.target_triple, "darwin") then
+    config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
+    config.font_size = 20
 else
-  -- TODO
+    -- TODO
 end
 
 return config
