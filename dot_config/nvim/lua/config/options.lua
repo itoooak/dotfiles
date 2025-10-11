9e492999f3522cf86133696a5b073eb2dcb59bf5
itoooak <101097771+itoooak@users.noreply.github.com>
2025-10-12 02:55:07 +0900

    nvim: Use pwsh as shell on Windows

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,14 +5,14 @@ opt.relativenumber = true
 
 opt.wrap = false
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = {
-		"markdown",
-		"typst",
-		"text",
-		"gitcommit",
-		"plaintex",
-	},
-	command = "setlocal wrap",
+    pattern = {
+        "markdown",
+        "typst",
+        "text",
+        "gitcommit",
+        "plaintex",
+    },
+    command = "setlocal wrap",
 })
 
 opt.cursorline = true
@@ -29,3 +29,14 @@ opt.ignorecase = true
 opt.smartcase = true
 
 opt.completeopt = "menuone,popup,noselect"
+
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+    -- :help shell-powershell
+    opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+    opt.shellcmdflag =
+    [[-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;]]
+    opt.shellredir = [[2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode]]
+    opt.shellpipe = [[2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode]]
+    opt.shellquote = ""
+    opt.shellxquote = ""
+end
