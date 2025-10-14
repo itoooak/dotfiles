2de47780421aef26ea7ddc1e51916732a318a712
itoooak <101097771+itoooak@users.noreply.github.com>
2025-10-14 18:21:08 +0900

    nvim: Add mini.surround, mini.hipatterns, mini.ai, mini.git

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -15,6 +15,30 @@ return {
         end, { desc = "Trim trailing space and last blank lines" })
 
         require("mini.pairs").setup()
+        require("mini.surround").setup()
+
+        local gen_ai_spec = require("mini.extra").gen_ai_spec
+        require("mini.ai").setup({
+            custom_textobjects = {
+                B = gen_ai_spec.buffer(),
+                D = gen_ai_spec.diagnostic(),
+                I = gen_ai_spec.indent(),
+                L = gen_ai_spec.line(),
+                N = gen_ai_spec.number(),
+            },
+        })
+
+        local hi_words = require("mini.extra").gen_highlighter.words
+        local hipatterns = require("mini.hipatterns")
+        hipatterns.setup({
+            highlighters = {
+                fixme = hi_words({ "FIXME", "Fixme", "fixme" }, "MiniHipatternsFixme"),
+                hack = hi_words({ "HACK", "Hack", "hack" }, "MiniHipatternsHack"),
+                todo = hi_words({ "TODO", "Todo", "todo" }, "MiniHipatternsTodo"),
+                note = hi_words({ "NOTE", "Note", "note" }, "MiniHipatternsNote"),
+                hex_color = hipatterns.gen_highlighter.hex_color(),
+            },
+        })
 
         require("mini.diff").setup({
             view = {
@@ -27,6 +51,9 @@ return {
             },
         })
 
+        require("mini.git").setup()
+        vim.keymap.set({ "n", "x" }, "<Leader>gs", "<Cmd>lua MiniGit.show_at_cursor()<CR>", { desc = "Show at cursor" })
+
         local miniclue = require("mini.clue")
         miniclue.setup({
             triggers = {
@@ -36,7 +63,6 @@ return {
 
                 -- Built-in completion
                 { mode = "i", keys = "<C-x>" },
-
                 -- `g` key
                 { mode = "n", keys = "g" },
                 { mode = "x", keys = "g" },
