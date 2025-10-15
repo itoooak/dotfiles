7b176583e4b11706f735936a73e86542af1a8061
itoooak <101097771+itoooak@users.noreply.github.com>
2025-10-15 17:46:59 +0900

    nvim: Remove auto startinsert on TermOpen

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,9 +1,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.api.nvim_create_autocmd("TermOpen", {
-    pattern = "*",
-    command = "startinsert",
-})
 
 require("config/options")
 require("config/keymaps")
