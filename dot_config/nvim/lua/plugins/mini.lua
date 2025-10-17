999028dccd3383e641224f71e5dbc6f2a02b640a
itoooak <101097771+itoooak@users.noreply.github.com>
2025-10-17 09:47:31 +0900

    nvim: Add keymaps in mini.files buffer

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -5,6 +5,39 @@ return {
 		require("mini.files").setup()
 		vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", { desc = "Open MiniFiles" })
 
+		-- Set focused directory as current working directory
+		local set_cwd = function()
+			local path = (MiniFiles.get_fs_entry() or {}).path
+			if path == nil then
+				return vim.notify("Cursor is not on valid entry")
+			end
+			vim.fn.chdir(vim.fs.dirname(path))
+		end
+
+		-- Yank in register full path of entry under cursor
+		local yank_path = function()
+			local path = (MiniFiles.get_fs_entry() or {}).path
+			if path == nil then
+				return vim.notify("Cursor is not on valid entry")
+			end
+			vim.fn.setreg(vim.v.register, path)
+		end
+
+		-- Open path with system default handler (useful for non-text files)
+		local ui_open = function()
+			vim.ui.open(MiniFiles.get_fs_entry().path)
+		end
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesBufferCreate",
+			callback = function(args)
+				local b = args.data.buf_id
+				vim.keymap.set("n", "g~", set_cwd, { buffer = b, desc = "Set cwd" })
+				vim.keymap.set("n", "go", ui_open, { buffer = b, desc = "OS open" })
+				vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
+			end,
+		})
+
 		require("mini.trailspace").setup()
 		vim.api.nvim_create_user_command("Trim", function()
 			MiniTrailspace.trim()
