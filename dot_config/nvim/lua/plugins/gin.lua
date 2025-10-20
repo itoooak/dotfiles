dd2e6b98f5a73a78a35379d5cd9ee196d130970a
itoooak <101097771+itoooak@users.noreply.github.com>
2025-10-20 19:36:15 +0900

    nvim: Add keymaps for vim-gin

--- dot_config/nvim/lua/plugins/gin.lua
+++ dot_config/nvim/lua/plugins/gin.lua
@@ -3,4 +3,20 @@ return {
 	dependencies = {
 		"vim-denops/denops.vim",
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>gb", "<cmd>GinBranch<CR>", { desc = "GinBranch" })
+		vim.keymap.set("n", "<leader>gB", "<cmd>GinBlame %<CR>", { desc = "GinBlame %" })
+		vim.keymap.set("n", "<leader>gc", "<cmd>Gin commit<CR>", { desc = "Gin commit" })
+		vim.keymap.set("n", "<leader>gd", "<cmd>GinDiff<CR>", { desc = "GinDiff" })
+		vim.keymap.set("n", "<leader>gD", "<cmd>GinDiff --staged<CR>", { desc = "GinDiff --staged" })
+		vim.keymap.set("n", "<leader>gl", "<cmd>GinLog<CR>", { desc = "GinLog" })
+		vim.keymap.set("n", "<leader>gs", "<cmd>GinStatus<CR>", { desc = "GinStatus" })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "gin-branch", "gin-diff", "gin-log", "gin-status" },
+			callback = function()
+				vim.keymap.set("n", "q", "<cmd>bdelete<CR>", { buffer = true })
+			end,
+		})
+	end,
 }
