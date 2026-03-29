90f01bd8c466f8dcecd27b49a638e73ef9b03107
itoooak <101097771+itoooak@users.noreply.github.com>
2026-03-29 20:06:07 +0900

    fix(nvim): load additional configuration modules in init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,4 +4,7 @@ vim.g.maplocalleader = "\\"
 require("config/options")
 require("config/keymaps")
 require("config/autocmds")
+require("config/clipboard")
+require("config/filetypes")
+require("config/platform")
 require("config/lazy")
