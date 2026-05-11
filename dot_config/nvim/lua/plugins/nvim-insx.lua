9e4581f14d16087a2460ad8c833ba903a9bd7a3e
itoooak <101097771+itoooak@users.noreply.github.com>
2026-05-11 10:25:29 +0900

    feat(nvim): disable single quote auto-pairing for specific filetypes

--- dot_config/nvim/lua/plugins/nvim-insx.lua
+++ dot_config/nvim/lua/plugins/nvim-insx.lua
@@ -2,5 +2,13 @@ return {
 	"hrsh7th/nvim-insx",
 	config = function()
 		require("insx.preset.standard").setup()
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "rust", "ocaml", "haskell", "lisp", "scheme" },
+			callback = function()
+				local opts = { buffer = true }
+				vim.keymap.set("i", "'", "'", opts)
+			end,
+		})
 	end,
 }
